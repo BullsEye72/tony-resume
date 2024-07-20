b5988b7e9c316ab7f9f6bbee7a34ae4b982eee89
Anthony Jaspers <jaspers.anthony@gmail.com>
2024-07-20 19:02:39 +0200

    added a forgotten gray background

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-   <div className="w-[210mm] h-[297mm] bg-white shadow-md mx-auto p-[20mm] box-border">
-    CV
-   </div>
+    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <div className="w-[210mm] h-[297mm] bg-white shadow-md mx-auto p-[20mm] box-border">CV</div>
+    </div>
   );
 }
