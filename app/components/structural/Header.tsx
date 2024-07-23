c4a20d1b2cdf0991941a4f1ef08c9e604a59f289
Anthony Jaspers <jaspers.anthony@gmail.com>
2024-07-23 10:28:17 +0200

    basic structure (header + content)

---
+++ app/components/structural/Header.tsx
@@ -0,0 +1,6 @@
+export default function Header() {
+  return <div className="flex flex-col p-0 w-full h-40 ">
+    <div className="grow">Header</div>
+  <div className="bg-blue-400 w-full h-16">
+    Separator</div></div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Content from "./components/structural/Content";
+import Header from "./components/structural/Header";
+
 export default function Home() {
   return (
     <div className="bg-gray-200 min-h-screen flex items-center justify-center">
-      <div className="w-[210mm] h-[297mm] bg-white shadow-md mx-auto p-[20mm] box-border">CV</div>
+      <div className="flex flex-col p-0 w-[210mm] h-[297mm] bg-white shadow-md mx-auto box-border ">
+        <Header />
+        <Content />
+      </div>
     </div>
   );
 }
